341fb5580e4deba3a565671ec55e249175d2e21f
seoly0 <seoly0.dev@gmail.com>
2024-06-06 10:24:34 +0900

    remove unnecessary annotations

--- src/main/kotlin/me/seoly/spring/jpa/BaseEntity.kt
+++ src/main/kotlin/me/seoly/spring/jpa/BaseEntity.kt
@@ -3,14 +3,10 @@ package me.seoly.spring.jpa
 import jakarta.persistence.*
 import org.hibernate.annotations.ColumnDefault
 import org.hibernate.annotations.CreationTimestamp
-import org.hibernate.annotations.DynamicInsert
-import org.hibernate.annotations.DynamicUpdate
 import org.hibernate.annotations.UpdateTimestamp
 import java.io.Serializable
 import java.time.LocalDateTime
 
-@DynamicInsert
-@DynamicUpdate
 @MappedSuperclass
 @Access(AccessType.FIELD)
 abstract class BaseEntity: Serializable {
